eda4e23136394b8dbb6be655f7eaa5d2f16ca98b
Arlo Bryer <arlo@quartic.io>
2017-08-25 10:09:26 +0100

    move implementation dependencies into extras.

--- setup.py
+++ setup.py
@@ -10,14 +10,18 @@ setup(name="quartic-python",
       packages=find_packages("src"),
       package_dir={"":"src"},
       install_requires=[
-          "requests==2.17.3",
           "pyaml==17.8.0",
-          "datadiff==2.0.0",
-          "networkx==1.11",
+
+          "networkx==1.11"
       ],
       extras_require={
           "graphviz":["pygraphviz==1.3.1"],
           "ipython":["ipython==6.0.0"],
-          "impl":["pyarrow==0.4.0", "pyproj==1.9.5.1", "pandas==0.20.1"]
+          "impl":[
+              "pyarrow==0.4.0",
+              "pyproj==1.9.5.1",
+              "pandas==0.20.1",
+              "requests==2.17.3",
+              "datadiff==2.0.0"]
       },
       scripts=["bin/qli"])
